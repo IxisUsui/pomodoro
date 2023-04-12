b801bb2804bd509ff2349bd8ae10c7952ed9ce77
ixiusui <usuitakumi86.dm@gmail.com>
2023-04-12 22:26:18 +0200

    dodanie redux

--- front/src/Components/mainPage.tsx
+++ front/src/Components/mainPage.tsx
@@ -6,11 +6,16 @@ import {useState} from "react";
 // @ts-ignore
 import SoundCorner from "./soundCorner.tsx";
 
+type Props = () => {
+    timerInMinutes: number;
+}
 const MainPage = () => {
+
+
     const [isSoundCornerActive, setIsSoundCornerActive] = useState<boolean>(true);
     const [isTaskCornerActive, setIsTaskCornerActive] = useState<boolean>(true);
     const [isChatCornerActive, setIsChatCornerActive] = useState<boolean>(true);
-
+    const [timeInMinutes, setTimeInMinutes] = useState<number>(15);
     const displayCorner = (cornerName: string) => {
         let btn = document.getElementById(cornerName);
         if (btn.classList.contains("activeBtn")) {
@@ -34,6 +39,19 @@ const MainPage = () => {
         displayCorner("chatCornerId");
     }
     const soundBarCornerDisplay = () =>{
+    }
+
+    const countingTime = () => {
+        console.log(timeInMinutes);
+        let time = timeInMinutes * 60;
+        const countDown = document.getElementById("countDown");
+        const  minutes = Math.floor(time / 60).toString();
+        let seconds = time % 60;
+        let secondsStr = '';
+        secondsStr = seconds < 10 ? '0' + seconds : seconds.toString();
+
+        countDown.innerText =`${minutes}:${secondsStr}`;
+        time--;
     }
 
 
@@ -66,10 +84,19 @@ const MainPage = () => {
                     }
 
                     <div className="timerPomodoro">
-                        <button className="buttonStyle">Long</button>
-                        <button className="buttonStyle">Short</button>
+                        <button className="buttonStyle" onClick={function() {
+                            setInterval(function(){
+                            countingTime();}, 1000);
+                        }
+                    }>Long</button>
+                        <button className="buttonStyle" onClick={function() {
+                            setTimeInMinutes(5);
+                            setInterval(function(){
+                                countingTime();}, 1000);
+                        }
+                        }>Short</button>
                         <button className="buttonStyle">Break</button>
-                        <h1>15:00</h1>
+                        <h1 id="countDown">15:00</h1>
                         <p id="lilText">Don't give up!</p>
                     </div>
 
