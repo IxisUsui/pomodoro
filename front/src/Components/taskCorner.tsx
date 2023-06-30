ada56c4583704ab73ed404f17d7852d3a1643c06
ixiusui <usuitakumi86.dm@gmail.com>
2023-06-30 22:32:54 +0200

    pom-front: adding task inputs

--- front/src/Components/taskCorner.tsx
+++ front/src/Components/taskCorner.tsx
@@ -1,14 +1,115 @@
 import * as React from 'react'
 import "../style/taskStyle.css";
 const TaskCorner = () => {
+
+    const addTask = () => {
+
+    }
+
+
     return (
         <div className="taskContainer">
             <div className="flex-column">
                 <div className="flex-column-task">
                     <h3>📋 Task corner</h3>
-                    <button>+</button>
                 </div>
-                <div className="flex-column-task">
+                <div id="task" className="flex-column-task">
+                    <div className="checkbox">
+                        <input type="checkbox" className="check" id="check2"/>
+                        <label htmlFor="check2" className="label">
+                            <svg width="50" height="50" viewBox="0 0 100 100">
+                                <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"/>
+                                <g transform="translate(0,-952.36222)">
+                                    <path
+                                        d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
+                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                </g>
+                            </svg>
+                        </label>
+                    </div>
+                    <input className="inputTask"></input>
+                </div>
+                <div id="task2" className="flex-column-task">
+                    <div className="checkbox">
+                        <input type="checkbox" className="check" id="check2"/>
+                        <label htmlFor="check2" className="label">
+                            <svg width="50" height="50" viewBox="0 0 100 100">
+                                <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"/>
+                                <g transform="translate(0,-952.36222)">
+                                    <path
+                                        d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
+                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                </g>
+                            </svg>
+                        </label>
+                    </div>
+                    <input className="inputTask"></input>
+                </div>
+                <div id="task3" className="flex-column-task">
+                    <div className="checkbox">
+                        <input type="checkbox" className="check" id="check2"/>
+                        <label htmlFor="check2" className="label">
+                            <svg width="50" height="50" viewBox="0 0 100 100">
+                                <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"/>
+                                <g transform="translate(0,-952.36222)">
+                                    <path
+                                        d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
+                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                </g>
+                            </svg>
+                        </label>
+                    </div>
+                    <input className="inputTask"></input>
+                </div>
+                <div id="task4" className="flex-column-task">
+                    <div className="checkbox">
+                        <input type="checkbox" className="check" id="check2"/>
+                        <label htmlFor="check2" className="label">
+                            <svg width="50" height="50" viewBox="0 0 100 100">
+                                <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"/>
+                                <g transform="translate(0,-952.36222)">
+                                    <path
+                                        d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
+                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                </g>
+                            </svg>
+                        </label>
+                    </div>
+                    <input className="inputTask"></input>
+                </div>
+                <div id="task5" className="flex-column-task">
+                    <div className="checkbox">
+                        <input type="checkbox" className="check" id="check2"/>
+                        <label htmlFor="check2" className="label">
+                            <svg width="50" height="50" viewBox="0 0 100 100">
+                                <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"/>
+                                <g transform="translate(0,-952.36222)">
+                                    <path
+                                        d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
+                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                </g>
+                            </svg>
+                        </label>
+                    </div>
+                    <input className="inputTask"></input>
+                </div>
+                <div id="task6" className="flex-column-task">
+                    <div className="checkbox">
+                        <input type="checkbox" className="check" id="check2"/>
+                        <label htmlFor="check2" className="label">
+                            <svg width="50" height="50" viewBox="0 0 100 100">
+                                <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"/>
+                                <g transform="translate(0,-952.36222)">
+                                    <path
+                                        d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
+                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                </g>
+                            </svg>
+                        </label>
+                    </div>
+                    <input className="inputTask"></input>
+                </div>
+                <div id="task7" className="flex-column-task">
                     <div className="checkbox">
                         <input type="checkbox" className="check" id="check2"/>
                         <label htmlFor="check2" className="label">
