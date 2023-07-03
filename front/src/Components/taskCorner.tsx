8717d232be04cbf277038facdb1cbc8406ebc097
ixiusui <usuitakumi86.dm@gmail.com>
2023-07-03 16:43:24 +0200

    pom-front: adding function to grayish text by checkbox checked

--- front/src/Components/taskCorner.tsx
+++ front/src/Components/taskCorner.tsx
@@ -6,28 +6,34 @@ const TaskCorner = () => {
 
     }
 
+    const changeColor = () => {
+        console.log("grayish text");
+    }
+
 
     return (
         <div className="taskContainer">
             <div className="flex-column">
+
                 <div className="flex-column-task">
                     <h3>📋 Task corner</h3>
+                    <button className="buttonStyle">Save</button>
                 </div>
-                <div id="task" className="flex-column-task">
+                <div className="flex-column-task">
                     <div className="checkbox">
-                        <input type="checkbox" className="check" id="check2"/>
+                        <input type="checkbox" className="check" id="check2" onClick={() => changeColor()}/>
                         <label htmlFor="check2" className="label">
                             <svg width="50" height="50" viewBox="0 0 100 100">
                                 <rect x="30" y="20" width="50" height="50" stroke="black" fill="none"/>
                                 <g transform="translate(0,-952.36222)">
                                     <path
                                         d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
-                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                        stroke="black" strokeWidth="3" fill="none" className="path1"/>
                                 </g>
                             </svg>
                         </label>
                     </div>
-                    <input className="inputTask"></input>
+                    <input id="task" className="inputTask"></input>
                 </div>
                 <div id="task2" className="flex-column-task">
                     <div className="checkbox">
@@ -38,7 +44,7 @@ const TaskCorner = () => {
                                 <g transform="translate(0,-952.36222)">
                                     <path
                                         d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
-                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                        stroke="black" strokeWidth="3" fill="none" className="path1"/>
                                 </g>
                             </svg>
                         </label>
@@ -54,7 +60,7 @@ const TaskCorner = () => {
                                 <g transform="translate(0,-952.36222)">
                                     <path
                                         d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
-                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                        stroke="black" strokeWidth="3" fill="none" className="path1"/>
                                 </g>
                             </svg>
                         </label>
@@ -70,7 +76,7 @@ const TaskCorner = () => {
                                 <g transform="translate(0,-952.36222)">
                                     <path
                                         d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
-                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                        stroke="black" strokeWidth="3" fill="none" className="path1"/>
                                 </g>
                             </svg>
                         </label>
@@ -86,7 +92,7 @@ const TaskCorner = () => {
                                 <g transform="translate(0,-952.36222)">
                                     <path
                                         d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
-                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                        stroke="black" strokeWidth="3" fill="none" className="path1"/>
                                 </g>
                             </svg>
                         </label>
@@ -102,7 +108,7 @@ const TaskCorner = () => {
                                 <g transform="translate(0,-952.36222)">
                                     <path
                                         d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
-                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                        stroke="black" strokeWidth="3" fill="none" className="path1"/>
                                 </g>
                             </svg>
                         </label>
@@ -118,7 +124,7 @@ const TaskCorner = () => {
                                 <g transform="translate(0,-952.36222)">
                                     <path
                                         d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4 "
-                                        stroke="black" stroke-width="3" fill="none" className="path1"/>
+                                        stroke="black" strokeWidth="3" fill="none" className="path1"/>
                                 </g>
                             </svg>
                         </label>
